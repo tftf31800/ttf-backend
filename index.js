c3e0d4dbc53b91399d59193e355c0562e3144e3e
tftf31800 <tftf31800@gmail.com>
2026-04-23 00:31:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -20,8 +20,6 @@ const transporter = nodemailer.createTransport({
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
-      tls: {
-    rejectUnauthorized: false,
   },
 });
 
