1c96b55efbf285236a8d6b5d441e587e8c6b4537
tftf31800 <tftf31800@gmail.com>
2026-04-22 23:54:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 const transporter = nodemailer.createTransport({
   host: "smtp.ionos.com",
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
